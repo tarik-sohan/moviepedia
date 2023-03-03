15e9aa3851335661088a00d94a9e3c765d2af3e8
Tarik Sohan <tarikjsohan@gmail.com>
2023-03-03 20:43:38 +0600

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
 function App() {
-  return <div>Testing...</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/starred" element={<Starred />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
